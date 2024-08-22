3eab5c09c712df111085b0aa771824be2789101b
abdullahek <30905026+abdullahek@users.noreply.github.com>
2024-08-22 22:15:14 +0500

    Update bot_view.py
    added logic for new coloumn

--- glogic/bot_view.py
+++ glogic/bot_view.py
@@ -21,12 +21,17 @@ def bot():
     resp = MessagingResponse()
     resp_start=request.form.get('Body').lower()
     yes_no=""
+    call_data=""
     if resp_start=="1":
         yes_no="Yes"
         resp.message("Great! Thank you for agreeing to participate!")
     elif resp_start=="2":
         yes_no="No"
         resp.message("Sad to hear that.")
+    elif resp_start=="a":
+        call_data="Yes"
+    elif resp_start=="b":
+        call_data="No"
     else:
         yes_no="invalid response"
         resp.message("Please enter a valid response 1 or 2")
@@ -37,7 +42,7 @@ def bot():
     result = validate_num(num) 
     if result:
         # resp.message("Thank you! Number is validated")
-        response= update_response(yes_no, num)
+        response= update_response(yes_no,call_data, num)
     else: 
         print("Number is not validated")
               
